e63f5f9d9f70a2f2c6b6a47aa2c38832a5f7a56f
martnx <martinmart2002@gmail.com>
2024-08-31 21:16:49 +0800

    version 1

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const container = document.querySelector(".container");
-
-
-//Create variable that it creates div with style of line 
-//I need 16x16 grid
-
-//The style can be change on style.css sheets
-
-let grid = 16;
-let gridReturn = grid * grid;
-
-for(let i = 0; i < 40; i++){
-    const createDiv = document.createElement("div");
-    createDiv.className = "div-col";
-    container.appendChild(createDiv);
+let size = 64;
+//Row and column loops
+for(let i = 0; i < size; i++){
+    console.log(i);
+    var row = document.createElement('div');
+    row.className = 'row';
+    container.appendChild(row);
+    //Column
+    for(let j = 0; j < size; j++){
+        var column = document.createElement('div');
+        column.className = 'column';
+        row.appendChild(column);  
+    }
 }
 
-console.log(gridReturn);
+//Make eventlistener for each column
+container.addEventListener("mousemove", e =>{
+    const target  = e.target;
+    console.log(e.target);
+    target.setAttribute("style", "background: black;");
+});
+
+const resetButton = document.querySelector('.reset-btn');
+resetButton.addEventListener("click", () =>{
+    column.forEach(element => {
+        column.setAttribute("style", "background-color: red");
+    console.log("click");
+    });
+    
+});
